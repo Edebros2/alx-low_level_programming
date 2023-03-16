9c6fb7dddbfa1aaa218339dd2286002cdbf10f03
Wisdom Ede <edewisdom001@gmail.com>
2023-03-16 09:42:09 -0700

    a c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(a);
+
+	return (0);
+}
