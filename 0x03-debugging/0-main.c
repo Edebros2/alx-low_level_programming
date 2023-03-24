dc69305342e67a1c576e9245defdec72d9c42b76
Wisdom Ede <edewisdom001@gmail.com>
2023-03-24 12:11:15 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * test function for positive or negative number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return(0);
+}
