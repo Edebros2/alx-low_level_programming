d917060472fa8383484ea07b5c1fc3e3ae5d71c7
Wisdom Ede <edewisdom001@gmail.com>
2023-03-24 16:57:19 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,15 +10,14 @@
 
 void print_most_numbers(void)
 {
-	int a;
+	char c;
 
-	for (a = 0; a <= 9; a++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (a != 2 || a != 4)
+		if (!(c == '2' || c == '4'))
 		{
-			_putchar((a % 10) + '0');
+			_putchar(c);
 		}
-
 	}
-	_putchar('\n');
+	__putchar('\n');
 }
