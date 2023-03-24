22d1fe798136ec0d517414368e1dc52b3e88e840
Wisdom Ede <edewisdom001@gmail.com>
2023-03-24 06:29:11 -0700

    a function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - Entry point
+ * @c: value to print
+ * function to print a character
+ *
+ * Return: Always c (Success)
+ */
+
+int _putchar(char c)
+{
+	return (c);
+}
