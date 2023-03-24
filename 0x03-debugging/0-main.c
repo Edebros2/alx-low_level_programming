4f543590b4a502600e3b6a16325fc7a030288cb0
Wisdom Ede <edewisdom001@gmail.com>
2023-03-24 13:43:20 -0700

    testing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,8 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * test function for positive or negative number
- *
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
@@ -15,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
