1dc55f5a232526b597de8dc0de9a1f0612b00c55
Wisdom Ede <edewisdom001@gmail.com>
2023-03-27 11:43:13 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ * a function that returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	m = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+		
