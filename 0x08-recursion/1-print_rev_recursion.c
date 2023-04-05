125745680e9abfee69229be76970730fb3b2d931
Wisdom Ede <edewisdom001@gmail.com>
2023-04-05 16:48:35 -0700

    a function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
 /**
  * _print_rev_recursion - a function that prints a string in reverse
  * @s: input string
- * @counter: moves the current character from the terminating null byte
+ * counter: moves the current character from the terminating null byte
  * to the character before it so that printing can start from there
  *
  * Return: s in reverse form
